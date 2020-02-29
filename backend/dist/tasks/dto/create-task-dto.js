fdbfc0b4f7a3274f1e0c7c96b7f4aea4fc5c4f36
Tiago Barreto <tiagobarreto@Tiagos-MacBook-Pro.local>
2020-02-28 23:48:47 -0300

    ...

--- backend/dist/tasks/dto/create-task-dto.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CreateTaskDTO {
-}
-exports.CreateTaskDTO = CreateTaskDTO;
-//# sourceMappingURL=create-task-dto.js.map
