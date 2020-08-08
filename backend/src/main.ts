7e0c106ebb7a4f3127b024f1b235ad94c184bf3d
Tiago Valentim Barreto <tiagovalentim@gmail.com>
2020-08-07 22:58:02 -0300

    Adding swager

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,8 @@
-import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common'; 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+import { Logger } from '@nestjs/common';
 import * as config from 'config';
 
 async function bootstrap() {
@@ -8,9 +10,18 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || config.get('server.port');
+
+  const options = new DocumentBuilder()
+    .setTitle('Tasks API')
+    .setDescription('The tasks API description')
+    .setVersion('1.0')
+    .addTag('tasks')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 
-  logger.verbose(`Application listening on port ${ port }`)
-
+  logger.verbose(`Application listening on port ${port}`);
 }
 bootstrap();
